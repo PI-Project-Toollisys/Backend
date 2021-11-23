d7a0cfc32dad4d3febf657433ba00ea0539aeb94
caiqueaugustocm <bispo.caique@hotmail.com>
2021-11-22 21:46:52 -0300

    Incompleto - Retorno dos comentários

--- routes/answerRoute.py
+++ routes/answerRoute.py
@@ -37,8 +37,11 @@ async def getAnswers(value):
 @answerAPI.get('/getScoreFirmByQuestion/{firm}/{quest}')
 # Get all answers in especific question
 async def getScoreAnswersByQuestion(firm,quest):
-    if not client.maindb.firm.find_one({"cnpj": firm}):
-        raise HTTPException(status_code=404, detail="Firm not found")
+    try:
+        if not client.maindb.firm.find_one({"cnpj": firm}):
+            raise HTTPException(status_code=404, detail="Firm not found")
+    except:
+        raise HTTPException(status_code=404, detail="Invalid CNPJ")
 
     n=client.maindb.answer.find({"firm": str(firm)}).count()
 
@@ -63,8 +66,11 @@ async def getScoreAnswersByQuestion(firm,quest):
 # que possui (relacionados a firma) dividir e obter a média
 # da pontuação
 async def getScoreTotal(firm):
-    if not client.maindb.firm.find_one({"cnpj": firm}):
-        raise HTTPException(status_code=404, detail="Firm not found")
+    try:
+        if not client.maindb.firm.find_one({"cnpj": firm}):
+            raise HTTPException(status_code=404, detail="Firm not found")
+    except:
+        raise HTTPException(status_code=404, detail="Invalid CNPJ")
 
     answers = answersEntity(client.maindb.answer.find({"firm": str(firm)}))
 
@@ -139,8 +145,11 @@ async def getScoreByQuestionSum(firm):
     "score_total": 0
     }
 
-    if not client.maindb.firm.find_one({"cnpj": firm}):
-        raise HTTPException(status_code=404, detail="Firm not found")
+    try:
+        if not client.maindb.firm.find_one({"cnpj": firm}):
+            raise HTTPException(status_code=404, detail="Firm not found")
+    except:
+        raise HTTPException(status_code=404, detail="Invalid CNPJ")
     
     answers = answersEntity(client.maindb.answer.find({"firm": str(firm)}))
 
@@ -158,6 +167,81 @@ async def getScoreByQuestionSum(firm):
 
     dictAux["score_total"] = float(dictAux["score_total"]/n)
     return dictAux
+
+
+@answerAPI.get('/getComments/{firm}')
+# Get all comments
+async def getComments(firm):
+    try:
+        if not client.maindb.firm.find_one({"cnpj": firm}):
+            raise HTTPException(status_code=404, detail="Firm not found")
+    except:
+        raise HTTPException(status_code=404, detail="Invalid CNPJ")
+
+    answers = answersEntity(client.maindb.answer.find({"firm": str(firm)}))
+
+    if not answers:
+        raise HTTPException(status_code=404, detail="Doesn't has answers")
+
+    n = client.maindb.answer.find({"firm": str(firm)}).count()
+
+    listComments = []
+    
+    dictAux = {
+        "firm": str(firm),
+        "rec":{
+            "title": "Reconhecimento",
+            "answers":[0,0,0,0,0]
+        },
+        "arm":{
+            "title": "Armamento",
+            "answers":[0,0,0]
+        },
+        "ent":{
+            "title": "Entrega",
+            "answers":[0,0,0,0,0]
+        },
+        "exp":{
+            "title": "Exploracao",
+            "answers":[0,0,0]
+        },
+        "ins":{
+            "title": "Instalacao",
+            "answers":[0,0,0]
+        },
+        "com":{
+            "title": "ComandoControle",
+            "answers":[0,0,0]
+        },
+        "aca":{
+            "title": "AcaoObjetivo",
+            "answers":[0,0,0]
+        },
+        "inft":{
+            "title": "Infraestrutura",
+            "answers":[0,0,0,0]
+        },
+        "infm":{
+            "title": "informacao",
+            "answers":[0,0,0]
+        },
+        "dad":{
+            "title": "Dados",
+            "answers":[0,0,0]   
+        }
+    }
+    
+    for doc in answers:
+        for item in doc:
+            if item == "id" or item == "firm" or item == "date":
+                pass
+            else:
+                dictAux[item]['answers'] = [ (a + b) for a, b in zip(dictAux[item]['answers'], doc[item]['answers']) ]
+            
+    if (dictAux["rec"]['answers'][0]/n) <= 0.7: #ERRADO
+        listComments.append({"code":"rec1","enunciado":"Você possui alguma rede social?","RespostaIdeal":"Não utilizo","Comentário":"BLA BLA BLA"},)
+
+    return 0
 
 
 @answerAPI.post('/postAnswer')
